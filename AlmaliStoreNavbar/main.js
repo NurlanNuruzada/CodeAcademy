1674a02d2bb611e9a86c1f3371d6fd0d6a61236e
unknown <nurlangn@code.edu.az>
2023-05-10 23:53:59 +0400

    fixed bugs

---
+++ AlmaliStoreNavbar/main.js
@@ -0,0 +1,5 @@
+const myMenu = document.querySelector('.main__nav2__menu')
+const BurgerMenu = document.querySelector('.burger')
+BurgerMenu.addEventListener('click', () =>{
+    myMenu.style.display ='flex'
+})
